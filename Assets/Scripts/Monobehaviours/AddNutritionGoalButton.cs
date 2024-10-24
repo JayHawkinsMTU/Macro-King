be3eb9a17934787b88d79e04ac65d1da212048af
jrhawkinMTU <hawkinsjr27@gmail.com>
2024-10-24 17:48:23 -0400

    Small additions to UI

---
+++ Assets/Scripts/Monobehaviours/AddNutritionGoalButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddNutritionGoalButton : MonoBehaviour
+{
+    public TMP_Text textInput;
+    public void AddGoal()
+    {
+        NutritionGoal goal = NutritionGoalContainer.goal;
+        goal.value = int.Parse(textInput.text);
+        User.instance.nutritionGoals.Add(goal);
+    }
+}
