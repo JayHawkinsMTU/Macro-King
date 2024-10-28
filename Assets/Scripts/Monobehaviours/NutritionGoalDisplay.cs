b5a5963dac579380187c22e06ed82dc423d47eb0
jrhawkinMTU <hawkinsjr27@gmail.com>
2024-10-28 13:17:22 -0400

    Working on connecting "Daily Nutrition Goal" with proper goal." to "Add
    Hoping to make one scene work for both adding and editing a goal.

--- Assets/Scripts/Monobehaviours/NutritionGoalDisplay.cs
+++ Assets/Scripts/Monobehaviours/NutritionGoalDisplay.cs
@@ -5,17 +5,29 @@ using TMPro;
 
 public class NutritionGoalDisplay : MonoBehaviour
 {
-    public NutritionGoal goal = new();
+    // When null, selecting will take you to create a new goal instead of editing
+    public NutritionGoal goal = null;
+    public ChangeSceneButton changeSceneButton;
     public TMP_Text goalTitle;
     public TMP_Text goalProgress;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(goal == null) 
+        {
+            return;
+        }
         string macro = NutritionGoal.macroToString[goal.macro];
         char cond = NutritionGoal.conditionToString[goal.condition];
         goalTitle.text = $"{macro}{cond}{goal.value}";
         // TODO: replace "0f" with today's amount of that macro.
         goalProgress.text = $"{0f}/{goal.value}";
+    }
+
+    public void Edit()
+    {
+        NutritionGoal.instance = this.goal;
+        changeSceneButton.ChangeScene();
     }
 }
