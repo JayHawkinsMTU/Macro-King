7e2ae24b8624b242fcf2dbad94ca702fa538ed84
Ridley Giannico <mcgianni@mtu.edu>
2024-11-10 23:30:43 -0500

    Safety Push
    Power outages so I want to push while i'm editing just in case

--- Assets/Scripts/DietLog/DietLog.cs
+++ Assets/Scripts/DietLog/DietLog.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Diet Log", menuName = "DietLog")]
 public class DietLog : ScriptableObject
 {
-    [SerializeField]  List<FoodItem> dietList = new List<FoodItem>();
-    public void addFoodItem(FoodItem f)
+    [SerializeField]  List<FoodEntry> dietList = new List<FoodEntry>();
+    public void addFoodItem(FoodEntry f)
     {
         dietList.Add(f);
     }
 
-    public FoodItem searchFoodItem(FoodItem f)
+    public FoodEntry searchFoodItem(FoodEntry f)
     {
         int i = dietList.IndexOf(f);
         if(i < 0) {return null; }
         return dietList[i];
     }
-    public List<FoodItem> getList() //testing purpose : remove later
+    public void removeFoodItem(FoodEntry f)
+    {
+        int i = dietList.IndexOf(f);
+        dietList.RemoveAt(i);
+    }
+    public List<FoodEntry> getList() //testing purpose : remove later
     {
         return dietList;
     }
